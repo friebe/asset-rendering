01d57d434ae3774483183ef75d20ad04f75b3e5f
Jan Friebe <friebe@odas.biz>
2018-12-07 08:35:14 +0100

    init

--- assets/dist/js/functions.js
+++ assets/dist/js/functions.js
@@ -1,1 +1,156 @@
-!function(e){var n={};function t(r){if(n[r])return n[r].exports;var o=n[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,t),o.l=!0,o.exports}t.m=e,t.c=n,t.d=function(e,n,r){t.o(e,n)||Object.defineProperty(e,n,{enumerable:!0,get:r})},t.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t.t=function(e,n){if(1&n&&(e=t(e)),8&n)return e;if(4&n&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(t.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&n&&"string"!=typeof e)for(var o in e)t.d(r,o,function(n){return e[n]}.bind(null,o));return r},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},t.p="",t(t.s=0)}([function(e,n,t){t(1),t(4),e.exports=t(5)},function(e,n,t){"use strict";t.r(n);t(2),t(3)},function(e,n){console.log("mein js plugin")},function(e,n){console.log("mein js plugin1")},function(e,n,t){e.exports=t.p+"dist/css/index.css"},function(e,n,t){e.exports=t.p+"dist/css/light.css"}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./assets/src/js/index.js":
+/*!********************************!*\
+  !*** ./assets/src/js/index.js ***!
+  \********************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _javascript_my_plugin__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./javascript-my-plugin */ \"./assets/src/js/javascript-my-plugin.js\");\n/* harmony import */ var _javascript_my_plugin__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_javascript_my_plugin__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _javascript_my_plugin1__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./javascript-my-plugin1 */ \"./assets/src/js/javascript-my-plugin1.js\");\n/* harmony import */ var _javascript_my_plugin1__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_javascript_my_plugin1__WEBPACK_IMPORTED_MODULE_1__);\n\r\n\n\n//# sourceURL=webpack:///./assets/src/js/index.js?");
+
+/***/ }),
+
+/***/ "./assets/src/js/javascript-my-plugin.js":
+/*!***********************************************!*\
+  !*** ./assets/src/js/javascript-my-plugin.js ***!
+  \***********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("console.log('mein js plugin');\n\n//# sourceURL=webpack:///./assets/src/js/javascript-my-plugin.js?");
+
+/***/ }),
+
+/***/ "./assets/src/js/javascript-my-plugin1.js":
+/*!************************************************!*\
+  !*** ./assets/src/js/javascript-my-plugin1.js ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("console.log('mein js plugin1');\n\n//# sourceURL=webpack:///./assets/src/js/javascript-my-plugin1.js?");
+
+/***/ }),
+
+/***/ "./assets/src/scss/index.scss":
+/*!************************************!*\
+  !*** ./assets/src/scss/index.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"dist/css/index.css\";\n\n//# sourceURL=webpack:///./assets/src/scss/index.scss?");
+
+/***/ }),
+
+/***/ "./assets/src/scss/themes/light.scss":
+/*!*******************************************!*\
+  !*** ./assets/src/scss/themes/light.scss ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__.p + \"dist/css/light.css\";\n\n//# sourceURL=webpack:///./assets/src/scss/themes/light.scss?");
+
+/***/ }),
+
+/***/ 0:
+/*!*******************************************************************************************************!*\
+  !*** multi ./assets/src/js/index.js ./assets/src/scss/index.scss ./assets/src/scss/themes/light.scss ***!
+  \*******************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("__webpack_require__(/*! ./assets/src/js/index.js */\"./assets/src/js/index.js\");\n__webpack_require__(/*! ./assets/src/scss/index.scss */\"./assets/src/scss/index.scss\");\nmodule.exports = __webpack_require__(/*! ./assets/src/scss/themes/light.scss */\"./assets/src/scss/themes/light.scss\");\n\n\n//# sourceURL=webpack:///multi_./assets/src/js/index.js_./assets/src/scss/index.scss_./assets/src/scss/themes/light.scss?");
+
+/***/ })
+
+/******/ });
